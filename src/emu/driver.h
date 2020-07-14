5a568758393f5cc63838f84cf93689324e244449
Timothy Stark <fsword007@gmail.com>
2020-07-14 19:04:37 -0400

    Updated some...

--- src/emu/driver.h
+++ src/emu/driver.h
@@ -9,16 +9,45 @@
 
 struct SystemDriver
 {
-	const char				*name;
-	const char				*parent;
-	const char				*section;
-//	const BaseDeviceType	&type;
+	const char			*name;
+	const char			*parent;
+	const char			*section;
+//	const DeviceType	&type;
 
 	const char   *description;
 	const char   *source;		// source file name
 };
 
+#define SYSTEM_NAME(Name) Name##_driver
+#define SYSTEM_EXTERN(Name) extern const SystemDriver SYSTEM_NAME(Name)
+#define SYSTEM_TRAITS_NAME(Name) Name##_system_traits
 
+#define SYSTEM_TRAITS(Name, FullName)							\
+	struct SYSTEM_TRAITS_NAME(Name) {							\
+		static constexpr tag_t shortName[]  = #Name;			\
+		static constexpr tag_t fullName[]   = FullName;			\
+		static constexpr tag_t fileName[]   = __FILE__;			\
+	};															\
+	constexpr tag_t SYSTEM_TRAITS_NAME(Name)::shortName[];		\
+	constexpr tag_t SYSTEM_TRAITS_NAME(Name)::fullName[];		\
+	constexpr tag_t SYSTEM_TRAITS_NAME(Name)::fileName[];
+
+#define SYSTEM_TYPE(Name, Class)					\
+	systemCreator<Class,							\
+		(SYSTEM_TRAITS_NAME(Name)::shortName),		\
+		(SYSTEM_TRAITS_NAME(Name)::fullName),		\
+		(SYSTEM_TRAITS_NAME(Name)::fileName)>
+
+#define COMP(Name, Parent, Section, Type, Class, Create, Init, Company, Description) \
+	SYSTEM_TRAITS(Name, Description) \
+	extern const SystemDriver SYSTEM_NAME(Name) = \
+	{ \
+		#Name, \
+		#Parent, \
+		#Section, \
+		Description, \
+		__FILE__ \
+	};
 
 //class system_driver
 //{
@@ -36,27 +65,8 @@ struct SystemDriver
 //	const char				*description;	//Description/full name
 //	const char				*source;		// source file name
 //};
-//
-//#define SYSTEM_NAME(Name) Name##_driver
-//#define SYSTEM_EXTERN(Name) extern const system_driver SYSTEM_NAME(Name)
-//#define SYSTEM_TRAITS_NAME(Name) Name##_system_traits
-//
-//#define SYSTEM_TRAITS(Name, FullName)							\
-//	struct SYSTEM_TRAITS_NAME(Name) {							\
-//		static constexpr tag_t shortName[]  = #Name;			\
-//		static constexpr tag_t fullName[]   = FullName;			\
-//		static constexpr tag_t sourceName[] = __FILE__;			\
-//	};															\
-//	constexpr tag_t SYSTEM_TRAITS_NAME(Name)::shortName[];		\
-//	constexpr tag_t SYSTEM_TRAITS_NAME(Name)::fullName[];		\
-//	constexpr tag_t SYSTEM_TRAITS_NAME(Name)::sourceName[];
-//
-//#define SYSTEM_TYPE(Name, Class)					\
-//	systemCreator<Class,							\
-//		(SYSTEM_TRAITS_NAME(Name)::shortName),		\
-//		(SYSTEM_TRAITS_NAME(Name)::fullName),		\
-//		(SYSTEM_TRAITS_NAME(Name)::sourceName)>
-//
+
+
 //#define COMP(Name, Parent, Section, Type, Class, Create, Init, Company, Description)	\
 //	SYSTEM_TRAITS(Name, Description)					\
 //	extern const system_driver SYSTEM_NAME(Name) =		\
