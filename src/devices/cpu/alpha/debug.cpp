d1b71b8f1ce13a9be49b5feec106f6fe15d42ab1
Timothy Stark <fsword007@gmail.com>
2020-11-21 15:43:27 -0500

    Alpha CPU Processor - new debugging routines

---
+++ src/devices/cpu/alpha/debug.cpp
@@ -0,0 +1,13 @@
+/*
+ * debug.cpp - Alpha CPU Processor - debugging routines
+ *
+ *  Created on: Nov 21, 2020
+ *      Author: Tim Stark
+ */
+
+#include "emu/core.h"
+#include "emu/sysconfig.h"
+#include "emu/device.h"
+#include "devices/cpu/alpha/axp.h"
+#include "devices/cpu/alpha/debug.h"
+
---
+++ src/devices/cpu/alpha/debug.h
@@ -0,0 +1,17 @@
+/*
+ * debug.h - Alpha CPU Processor - debugging routines
+ *
+ *  Created on: Nov 21, 2020
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+#ifdef DEBUG
+
+
+#else
+
+#define OP(opcode, format) DO_##opcode
+
+#endif
