e8730a09c22ba540b4c012e3b9aa60a2ce8fef02
Timothy Stark <fsword007@gmail.com>
2020-11-22 10:01:40 -0500

    Alpha CPU Processor - new two 21264 header files

---
+++ src/devices/cpu/alpha/21264.h
@@ -0,0 +1,8 @@
+/*
+ * 21264.h - 21264 Alpha EV6 Processor
+ *
+ *  Created on: Nov 22, 2020
+ *      Author: Tim Stark
+ */
+
+#pragma once
---
+++ src/devices/cpu/alpha/21264cb.h
@@ -0,0 +1,10 @@
+/*
+ * 21264.h - 21264CB Alpha EV68 Processor
+ *
+ *  Created on: Nov 22, 2020
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+#define CPU_TYPE_EV68
