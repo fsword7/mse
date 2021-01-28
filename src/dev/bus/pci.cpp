063453c236f507081a3962cb23524e412eca92c9
Timothy Stark <fsword007@gmail.com>
2021-01-28 15:41:25 -0500

    Initial PCI bus system package

---
+++ src/dev/bus/pci.cpp
@@ -0,0 +1,14 @@
+/*
+ * pci.cpp - Intel PCI
+ *
+ *  Created on: Jan 28, 2021
+ *      Author: Tim Stark
+ */
+
+#include "emu/core.h"
+#include "dev/bus/pci.h"
+
+
+
+
+
---
+++ src/dev/bus/pci.h
@@ -0,0 +1,18 @@
+/*
+ * pci.h - PCI device bus
+ *
+ *  Created on: Jan 28, 2021
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+
+class pci_BusDevice : public Device
+{
+public:
+	pci_BusDevice() = default;
+	~pci_BusDevice() = default;
+
+private:
+};
