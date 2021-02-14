6edad8f6e04c9cdd97f55e1984c168d4e0d0a491
Timothy Stark <fsword007@gmail.com>
2021-02-14 15:53:55 -0500

    Added PDP-10 processor emulation package.

---
+++ src/dev/cpu/pdp10/pdp10.cpp
@@ -0,0 +1,10 @@
+/*
+ * pdp10.cpp - DEC PDP-10 processor emulation package
+ *
+ *  Created on: Feb 14, 2021
+ *      Author: Tim Stark
+ */
+
+#include "emu/core.h"
+#include "dev/cpu/pdp10/pdp10.h"
+
---
+++ src/dev/cpu/pdp10/pdp10.h
@@ -0,0 +1,15 @@
+/*
+ * pdp10.h - DEC PDP-10 processor emulation package
+ *
+ *  Created on: Feb 14, 2021
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+#include "emu/devproc.h"
+
+class pdp10_cpuDevice : public cpuDevice
+{
+
+};
