c8a4b6d33feea724555cb9e6830a41d1d30665e6
Timothy Stark <fsword007@gmail.com>
2021-06-24 11:20:13 -0400

    Added 8080/8085 processor emulation package.

---
+++ src/dev/cpu/i8080/i8080.cpp
@@ -0,0 +1,10 @@
+/*
+ * i8080.cpp - Intel 8080/8085 processor emulation package
+ *
+ *  Created on: Jun 24, 2021
+ *      Author: Tim Stark
+ */
+
+#include "emu/core.h"
+#include "dev/cpu/i8080/i8080.h"
+
---
+++ src/dev/cpu/i8080/i8080.h
@@ -0,0 +1,15 @@
+/*
+ * i8080.h - Intel 8080/8085 processor emulation package
+ *
+ *  Created on: Jun 24, 2021
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+#include "emu/devproc.h"
+
+class i8080_cpuDevice : public cpuDevice
+{
+
+};
