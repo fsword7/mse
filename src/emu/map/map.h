9d7d7b493b2541db6a39f367d71a41a6ab90633c
Timothy Stark <fsword007@gmail.com>
2021-08-17 19:46:45 -0400

    Added memory cache access package.

--- src/emu/map/map.h
+++ src/emu/map/map.h
@@ -125,71 +125,9 @@ namespace aspace
 	using cAddressConfig = const AddressConfig;
 
 	class AddressList;
+	class AddressSpace;
 	class BusManager;
 
-	class AddressSpace
-	{
-	public:
-		AddressSpace(BusManager &manager, diExternalBus &bus, int space);
-
-		virtual ~AddressSpace() = default;
-
-		inline const AddressConfig &getConfig() { return config; }
-
-		inline uint16_t getDataWidth() const { return config.getDataWidth(); }
-		inline uint16_t getDataRadix() const { return config.getDataRadix(); }
-		inline uint16_t getAddrWidth() const { return config.getAddrWidth(); }
-		inline uint16_t getAddrRadix() const { return config.getAddrRadix(); }
-		inline int16_t  getAddrShift() const { return config.getAddrShift(); }
-		inline int16_t  getPageShift() const { return config.getPageShift(); }
-		inline offs_t   getAddrMask() const  { return addrMask; }
-		inline uint64_t getUnmapped() const  { return unmapValue; }
-
-		// Unmapped value setting
-		inline void setUnmapLowValue()          { unmapValue = 0; }
-		inline void setUnmapHighValue()         { unmapValue = ~0ull; }
-		inline void setUnmapValue(uint64_t val) { unmapValue = val; }
-
-		// Setup initialization routines
-		void prepare(Console *cty);
-		void populate(Console *cty);
-		void allocate(Console *cty);
-
-		// Virtual function calls
-		virtual uint8_t  read8(offs_t addr, cpuDevice *cpu = nullptr) = 0;
-		virtual uint16_t read16(offs_t addr, cpuDevice *cpu = nullptr) = 0;
-		virtual uint16_t read16u(offs_t addr, cpuDevice *cpu = nullptr) = 0;
-		virtual uint32_t read32(offs_t addr, cpuDevice *cpu = nullptr) = 0;
-		virtual uint32_t read32u(offs_t addr, cpuDevice *cpu = nullptr) = 0;
-		virtual uint64_t read64(offs_t addr, cpuDevice *cpu = nullptr) = 0;
-		virtual uint64_t read64u(offs_t addr, cpuDevice *cpu = nullptr) = 0;
-		virtual void     readBlock(offs_t addr, uint8_t *data, uint64_t size) = 0;
-
-		virtual void write8(offs_t addr, uint8_t data, cpuDevice *cpu = nullptr) = 0;
-		virtual void write16(offs_t addr, uint16_t data, cpuDevice *cpu = nullptr) = 0;
-		virtual void write16u(offs_t addr, uint16_t data, cpuDevice *cpu = nullptr) = 0;
-		virtual void write32(offs_t addr, uint32_t data, cpuDevice *cpu = nullptr) = 0;
-		virtual void write32u(offs_t addr, uint32_t data, cpuDevice *cpu = nullptr) = 0;
-		virtual void write64(offs_t addr, uint64_t data, cpuDevice *cpu = nullptr) = 0;
-		virtual void write64u(offs_t addr, uint64_t data, cpuDevice *cpu = nullptr) = 0;
-		virtual void writeBlock(offs_t addr, uint8_t *data, uint64_t size) = 0;
-
-	protected:
-		cAddressConfig &config;
-		Device         &device;
-		BusManager     &manager;
-		int             space;
-
-		AddressList    *map = nullptr;
-
-		offs_t addrMask = 0;
-
-		uint64_t unmapValue = 0;
-
-		// Optional main memory space
-		uint8_t *memData = nullptr;
-		uint64_t memSize = 0;
-	};
 
 	template <int Level, int dWidth, int aShift, endian_t type>
 	class MemoryAccessSpecific
@@ -353,13 +291,13 @@ namespace aspace
 		}
 
 	private:
-		void set(AddressSpace *space, const void *read, const void *write)
-		{
-			this->space = space;
-			this->addrMask = space->getAddrMask();
-			this->readDispatch = (const HandlerRead<dWidth, aShift, type> *const *)(read);
-			this->writeDispatch = (const HandlerWrite<dWidth, aShift, type> *const *)(write);
-		}
+		void set(AddressSpace *space, const void *read, const void *write);
+		// {
+		// 	this->space = space;
+		// 	// this->addrMask = space->getAddrMask();
+		// 	this->readDispatch = (const HandlerRead<dWidth, aShift, type> *const *)(read);
+		// 	this->writeDispatch = (const HandlerWrite<dWidth, aShift, type> *const *)(write);
+		// }
 
 	private:
 		AddressSpace *space = nullptr;
@@ -373,12 +311,271 @@ namespace aspace
 		uint8_t *memData = nullptr;
 	};
 
-	template <int highBits, int dWidth, int aShift, endian_t Endian>
+	template <int dWidth, int aShift, endian_t type>
+	class MemoryAccessCache
+	{
+		friend class AddressSpace;
+
+		using uintx_t = typename HandlerSize<dWidth>::uintx_t;
+
+		static constexpr uint64_t nativeBytes = 1 << dWidth;
+		static constexpr uint64_t nativeMask = dWidth - aShift >= 0 ? (1u << (dWidth - aShift)) - 1 : 0;
+
+	public:
+		MemoryAccessCache() = default;
+
+		inline AddressSpace &getSpace() { return *space; }
+		// **** Read access function calls
+
+		uint8_t read8(offs_t addr, cpuDevice *cpu)
+		{
+			if (addr < memSize)
+				return memData[addr];
+
+			// Check memory block ranges
+		//	for (int idx = 0; idx < mapMemories.size(); idx++)
+		//	{
+		//		mapMemory *mem = mapMemories[idx];
+		//
+		//		if (addr >= mem->baseAddr && addr < mem->endAddr)
+		//			return mem->data[addr - mem->baseAddr];
+		//	}
+
+			return unmapValue;
+		}
+
+		uint16_t read16(offs_t addr, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + (addr & ~0x1);
+				return *((uint16_t *)ptr);
+			}
+			return unmapValue;
+		}
+
+		uint16_t read16u(offs_t addr, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + addr;
+				return *((uint16_t *)ptr);
+			}
+			return unmapValue;
+		}
+
+		uint32_t read32(offs_t addr, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + (addr & ~0x3);
+				return *((uint32_t *)ptr);
+			}
+			return unmapValue;
+		}
+
+		uint32_t read32u(offs_t addr, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + addr;
+				return *((uint32_t *)ptr);
+			}
+			return unmapValue;
+		}
+
+		uint64_t read64(offs_t addr, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + (addr & ~0x7);
+				return *((uint64_t *)ptr);
+			}
+			return unmapValue;
+		}
+
+		uint64_t read64u(offs_t addr, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + addr;
+				return *((uint64_t *)ptr);
+			}
+			return unmapValue;
+		}
+
+		void readBlock(offs_t addr, uint8_t *data, uint64_t size)
+		{
+			if (addr < memSize) {
+				if ((addr + size) > memSize)
+					size = memSize - addr;
+				memcpy(data, &memData[addr], size);
+			}
+		}
+
+		// **** Write access function calls ****
+
+		void write8(offs_t addr, uint8_t data, cpuDevice *cpu)
+		{
+			if (addr < memSize)
+				memData[addr] = data;
+		}
+
+		void write16(offs_t addr, uint16_t data, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + (addr & ~0x1);
+				*((uint16_t *)ptr) = data;
+			}
+		}
+
+		void write16u(offs_t addr, uint16_t data, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + addr;
+				*((uint16_t *)ptr) = data;
+			}
+		}
+
+		void write32(offs_t addr, uint32_t data, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + (addr & ~0x2);
+				*((uint32_t *)ptr) = data;
+			}
+		}
+
+		void write32u(offs_t addr, uint32_t data, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + addr;
+				*((uint32_t *)ptr) = data;
+			}
+		}
+
+		void write64(offs_t addr, uint64_t data, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + (addr & ~0x3);
+				*((uint64_t *)ptr) = data;
+			}
+		}
+
+		void write64u(offs_t addr, uint64_t data, cpuDevice *cpu)
+		{
+			if (addr < memSize) {
+				uint8_t *ptr = memData + addr;
+				*((uint64_t *)ptr) = data;
+			}
+		}
+
+		void writeBlock(offs_t addr, uint8_t *data, uint64_t size)
+		{
+			if (addr < memSize) {
+				if ((addr + size) > memSize)
+					size = memSize - addr;
+				memcpy(&memData[addr], data, size);
+			}
+		}
+
+	private:
+		void set(AddressSpace *space, const void *read, const void *write);
+		// {
+		// 	this->space = space;
+		// 	// this->addrMask = space->getAddrMask();
+		// 	this->readDispatch = (const HandlerRead<dWidth, aShift, type> *const *)(read);
+		// 	this->writeDispatch = (const HandlerWrite<dWidth, aShift, type> *const *)(write);
+		// }
+
+	private:
+		AddressSpace *space = nullptr;
+		offs_t addrMask = 0;
+		uint64_t unmapValue = 0;
+
+		const HandlerRead<dWidth, aShift, type> *readDispatch = nullptr;
+		const HandlerWrite<dWidth, aShift, type> *writeDispatch = nullptr;
+
+		offs_t   memSize = 0;
+		uint8_t *memData = nullptr;
+	};
+
+	template <int highBits, int dWidth, int aShift, endian_t type>
 	struct MemoryAccess
 	{
 		static constexpr int Level = determineLevel(highBits);
 
-		using specific = MemoryAccessSpecific<Level, dWidth, aShift, Endian>;
+		using specific = MemoryAccessSpecific<Level, dWidth, aShift, type>;
+		using cache = MemoryAccessCache<dWidth, aShift, type>;
+	};
+
+	class AddressSpace
+	{
+	public:
+		AddressSpace(BusManager &manager, diExternalBus &bus, int space);
+
+		virtual ~AddressSpace() = default;
+
+		inline const AddressConfig &getConfig() { return config; }
+
+		inline uint16_t getDataWidth() const { return config.getDataWidth(); }
+		inline uint16_t getDataRadix() const { return config.getDataRadix(); }
+		inline uint16_t getAddrWidth() const { return config.getAddrWidth(); }
+		inline uint16_t getAddrRadix() const { return config.getAddrRadix(); }
+		inline int16_t  getAddrShift() const { return config.getAddrShift(); }
+		inline int16_t  getPageShift() const { return config.getPageShift(); }
+		inline offs_t   getAddrMask() const  { return addrMask; }
+		inline uint64_t getUnmapped() const  { return unmapValue; }
+
+		// Unmapped value setting
+		inline void setUnmapLowValue()          { unmapValue = 0; }
+		inline void setUnmapHighValue()         { unmapValue = ~0ull; }
+		inline void setUnmapValue(uint64_t val) { unmapValue = val; }
+
+		// Setup initialization routines
+		void prepare(Console *cty);
+		void populate(Console *cty);
+		void allocate(Console *cty);
+
+		template <int Level, int dWidth, int aShift, endian_t type>
+		void setMemorySpecific(MemoryAccessSpecific<Level, dWidth, aShift, type> memAccess)
+		{
+
+		}
+
+		template <int dWidth, int aShift, endian_t type>
+		void setMemoryCache(MemoryAccessCache<dWidth, aShift, type> memAccess)
+		{
+
+		}
+
+		// Virtual function calls
+		virtual uint8_t  read8(offs_t addr, cpuDevice *cpu = nullptr) = 0;
+		virtual uint16_t read16(offs_t addr, cpuDevice *cpu = nullptr) = 0;
+		virtual uint16_t read16u(offs_t addr, cpuDevice *cpu = nullptr) = 0;
+		virtual uint32_t read32(offs_t addr, cpuDevice *cpu = nullptr) = 0;
+		virtual uint32_t read32u(offs_t addr, cpuDevice *cpu = nullptr) = 0;
+		virtual uint64_t read64(offs_t addr, cpuDevice *cpu = nullptr) = 0;
+		virtual uint64_t read64u(offs_t addr, cpuDevice *cpu = nullptr) = 0;
+		virtual void     readBlock(offs_t addr, uint8_t *data, uint64_t size) = 0;
+
+		virtual void write8(offs_t addr, uint8_t data, cpuDevice *cpu = nullptr) = 0;
+		virtual void write16(offs_t addr, uint16_t data, cpuDevice *cpu = nullptr) = 0;
+		virtual void write16u(offs_t addr, uint16_t data, cpuDevice *cpu = nullptr) = 0;
+		virtual void write32(offs_t addr, uint32_t data, cpuDevice *cpu = nullptr) = 0;
+		virtual void write32u(offs_t addr, uint32_t data, cpuDevice *cpu = nullptr) = 0;
+		virtual void write64(offs_t addr, uint64_t data, cpuDevice *cpu = nullptr) = 0;
+		virtual void write64u(offs_t addr, uint64_t data, cpuDevice *cpu = nullptr) = 0;
+		virtual void writeBlock(offs_t addr, uint8_t *data, uint64_t size) = 0;
+
+	protected:
+		cAddressConfig &config;
+		Device         &device;
+		BusManager     &manager;
+		int             space;
+
+		AddressList    *map = nullptr;
+
+		offs_t addrMask = 0;
+
+		uint64_t unmapValue = 0;
+
+		// Optional main memory space
+		uint8_t *memData = nullptr;
+		uint64_t memSize = 0;
 	};
 
 	class MemoryBlock
@@ -473,6 +670,28 @@ namespace aspace
 		uint8_t  *memData = nullptr;
 		uint64_t  memSize = 0;
 	};
+
+
+	template <int Level, int dWidth, int aShift, endian_t type>
+	void MemoryAccessSpecific<Level, dWidth, aShift, type>::
+	set(AddressSpace *space, const void *read, const void *write)
+	{
+		this->space = space;
+		this->addrMask = space->getAddrMask();
+		this->readDispatch = (const HandlerRead<dWidth, aShift, type> *const *)(read);
+		this->writeDispatch = (const HandlerWrite<dWidth, aShift, type> *const *)(write);
+	}
+
+	template <int dWidth, int aShift, endian_t type>
+	void MemoryAccessCache<dWidth, aShift, type>::
+	set(AddressSpace *space, const void *read, const void *write)
+	{
+		this->space = space;
+		this->addrMask = space->getAddrMask();
+		this->readDispatch = (const HandlerRead<dWidth, aShift, type> *const *)(read);
+		this->writeDispatch = (const HandlerWrite<dWidth, aShift, type> *const *)(write);
+	}
+
 }
 
 using mapSpaceType     = aspace::SpaceType;
