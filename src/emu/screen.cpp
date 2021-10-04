18ad331d487f394c1d3d99e1751b4301c641394d
Timothy Stark <fsword007@gmail.com>
2021-10-04 11:34:01 -0400

    New screen package for video controllers.

---
+++ src/emu/screen.cpp
@@ -0,0 +1,9 @@
+/*
+ *  screen.cpp - screen package for video controllers
+ *
+ *  Created on: Oct 4, 2021
+ *      Author: Tim Stark
+ */
+
+#include "emu/core.h"
+#include "emu/screen.h"
---
+++ src/emu/screen.h
@@ -0,0 +1,8 @@
+/*
+ *  screen.h - screen package for video controllers
+ *
+ *  Created on: Oct 4, 2021
+ *      Author: Tim Stark
+ */
+
+#pragma once
